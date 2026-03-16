ada74a0e3ae0da2909b450a69f0549e08247fb77
Dingling Yao <ddcoan@icloud.com>
2026-03-16 22:43:23 +0100

    fix root dir

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',   // <-- change to your repo name
+  base: '/',   // <-- change to your repo name
 })
